f7c9ed3e59a7f4b46ba8a136aecef41ea7da293f
Laisson R. Silveira <laisson.r.silveira@gmail.com>
2021-08-15 18:33:16 -0300

    Update

---
+++ JavaScript/triplets.js
@@ -0,0 +1,36 @@
+'use strict';
+const assert = require('assert');
+
+function triplets(t, d) {
+    let attempts = 0;
+    let sizeArray = d.length;
+    // d.sort((a, b) => a - b);
+    for (let i = 0; i < sizeArray - 2; i++) {
+        for (let j = i + 1; j < sizeArray - 1; j++) {
+            for (let k = j + 1; k < sizeArray; k++) {
+                // console.log(`(${d[i]}, ${d[j]}, ${d[k]})`);
+                if (d[i] < d[j] < d[k] && d[i] + d[j] + d[k] <= t)
+                    attempts++;
+            }
+        }
+    }
+    return attempts;
+}
+
+function test(sum, array, expected) {
+    console.log('-------------------------------------');
+    console.log('IN', sum, '->', array);
+    const label = 'Total time';
+    console.time(label);
+    setTimeout(() => process.exit(1), 10000);
+    const result = triplets(sum, array);
+    console.timeEnd(label);
+    console.log('\nOUT', result);
+    assert.equal(result, expected);
+    console.log('-------------------------------------');
+}
+
+test(8, [1, 2, 3, 4, 6], 3);
+test(500, Array.from({ length: 2000 }, (_, i) => i + 1), 3420347);
+
+console.log('Tests Success!');
--- README.md
+++ README.md
@@ -3,8 +3,9 @@
 ## Java
 
 * [PromotionalEvents](Java/src/main/java/PromotionalEvents.java)
-* [Turnstile](Java/src/main/java/Turnstile.java)
+* [~~Turnstile~~](Java/src/main/java/Turnstile.java)
 
 ## JavaScript
 
-* [Triplets](JavaScript/triplets_challange.js)
+* [Triplets](JavaScript/triplets.js)
+* [Insert Sort Part 01](JavaScript/insert_sort_pt1.js)
